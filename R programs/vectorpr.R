cbfab96259901fa3a0c83928cbab7f488cf28b43
pathanzaki <159039350+pathanzaki@users.noreply.github.com>
2025-06-05 16:03:00 +0530

    Add files via upload

---
+++ R programs/vectorpr.R
@@ -0,0 +1,9 @@
+num_vec <- c(1, 2, 3, 4.5)
+int_vec <- c(1L, 2L, 3L)
+char_vec <- c("apple", "banana", "cherry")
+log_vec <- c(TRUE, FALSE, TRUE)
+
+class(num_vec)     # "numeric"
+typeof(int_vec)    # "integer"
+length(char_vec)   # 3
+str(log_vec)       # atomic vector of logicals
